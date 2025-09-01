635168ae372790fdd587b22119966a81cff408d1
XENITz <imanolmhz@gmail.com>
2025-09-01 03:34:00 -0400

    ADD SERVER FILE

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, jsonify
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detection")
+
+@app.route("/emotionDetector")
+def sent_emotion():
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+
+    for i in response:
+
+        answer = f"For the given statement, the system response is\n"
+        for emo in response:
+            if emo !=  "dominant_emotion":
+                answer += f"{emo}: {response[emo]}\n"
+            else:
+                answer += f"The dominant emotion is: {response[emo]}"
+    return answer
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5003)
